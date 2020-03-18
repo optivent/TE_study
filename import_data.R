5c685f4b82cef2c61a0d236cc45c18fe981e9457
I G <igeorgevici@outlook.com>
2020-03-18 23:50:14 +0100

    some major changes

--- import_data.R
+++ import_data.R
@@ -6,6 +6,7 @@
   # use_github(protocol = "https", auth_token = Sys.getenv("GITHUB_PAT"))
   # install.packages("here")
 }
+
 clean.it <- function() {
   basic.packages <- c("package:stats","package:graphics",
                       "package:grDevices","package:utils",
@@ -31,72 +32,137 @@ clean.it <- function() {
 clean.it()
 
 spss_data_n158 <- read_sav("input/Datensatz_TO_TE_komplett_neu_n158.sav") %>% as_tibble() 
-dim(spss_data_n158)
+all_columns <- spss_data_n158 %>% colnames() 
+dim(spss_data_n158) # 307 columns
 
-spss_data_n158 %<>% janitor::clean_names() %>% janitor::remove_empty(c("rows", "cols")) 
-dim(spss_data_n158)
+spss_data_n158 %<>% janitor::remove_empty(c("rows", "cols")) 
+dim(spss_data_n158) # 245 columns
+#setdiff(all_columns, colnames(spss_data_n158)) %>% enframe() %>% View()
+
+spss_data_n158 %<>% janitor::clean_names() 
 
 spss_data_n158 %<>% rename(age_in_months = alter_monate, gender = geschlecht, weight = gewicht)
-
 colnames(spss_data_n158) <- colnames(spss_data_n158) %>% str_replace("x", "") %>% str_to_sentence()
 dim(spss_data_n158)
 
-sapply(spss_data_n158, function(x) sum(is.na(x))) %>% enframe() %>%
-  mutate(percent = (100*value)/nrow(spss_data_n158)) %>% 
-  arrange(desc(percent)) %>% View()
+library(purrr)
+labels_list <- spss_data_n158 %>% select_if(is.labelled) %>% 
+  map(function(x) attr(x, 'labels')) 
 
-
-threshold <- 72 # discard columns with more than 72 missing values
-
-spss_data_n158  %<>% purrr::discard(~sum(is.na(.x))/length(.x)* 100 >= threshold) 
-dim(spss_data_n158)
+# sapply(spss_data_n158, function(x) sum(is.na(x))) %>% enframe() %>%
+#   mutate(percent_missing = (100*value)/nrow(spss_data_n158)) %>% 
+#   arrange(desc(percent_missing)) %>% View()
+# threshold <- 72 # discard columns with more than 72 procent missing values
+# spss_data_n158  %<>% purrr::discard(~sum(is.na(.x))/length(.x)* 100 >= threshold) 
+# dim(spss_data_n158)
 
 spss_data_n158 %<>% unite("ID", 1:3, sep = "_", remove = TRUE, na.rm = FALSE)
 dim(spss_data_n158)
 
-dipidolor <- spss_data_n158 %>% dplyr::select(matches("ID|Dipi|dipi"))
-colnames(dipidolor)
-#tail(colnames(dipidolor))
 
-dipidolor %<>% dplyr::select(-c(2,3,42:44)) 
-dipidolor %<>% dplyr::select(-matches("mg|Indikation")) 
-colnames(dipidolor) %>% enframe() 
-
-dipidolor  %<>%  
+dipidolor <- spss_data_n158 %>% 
+  dplyr::select(matches("ID|Dipi|dipi")) %>% 
+  dplyr::select(-matches("4_|5_tag|Indikation")) %>% 
+  dplyr::select(-c("Dipi_0nein_1ja","Haufigkeit_der_dipigabe")) %>% 
+  select_all(~str_replace_all(., "Awr", "0_0")) %>% 
+  select_all(~str_replace_all(., "4h", "0_1")) %>% 
+  select_all(~str_replace_all(., "8h", "0_2")) %>% 
+  select_all(~str_replace_all(., "dipi", "")) %>% 
   select_all(~str_replace_all(., "tag_mo", "0")) %>%
   select_all(~str_replace_all(., "tag_mi", "1")) %>%
   select_all(~str_replace_all(., "tag_a", "2")) %>% 
-  select_all(~str_replace_all(., "Awr_dipi_1ja", "0_0")) %>% 
-  select_all(~str_replace_all(., "4h_dipi_gabe_ja_nein", "0_1")) %>% 
-  select_all(~str_replace_all(., "8h_dipi_gabe_ja_nein", "0_2")) %>% 
-  select_all(~str_replace_all(., "_dipi_gabe_ja_nein", "")) %>% 
-  select_all(~str_replace_all(., "_dipi", ""))
+  select_all(~str_replace_all(., "__", "_")) %>% 
+  dplyr::rename('2_0_gabe_ja_nein' = '2_0_')
+
+dipidolor_quantitative <- dipidolor %>% dplyr::select(matches("ID|mg")) 
+dipidolor_quantitative %<>% rowwise() %>% mutate_at( vars(-ID), ~ ifelse(is.na(.), NA, ifelse(. > 0, 1, .)) ) 
+dipidolor_quantitative %<>% pivot_longer(-ID) %>% 
+  mutate(time = str_sub(name, 1, 3)) %>% 
+  group_by(ID, time) %>% 
+  summarise(value_quan = as.integer(sum(value, na.rm = TRUE))) %>% 
+  arrange(ID, time)
+dipidolor_quantitative$time %>% unique()
+head(dipidolor_quantitative,10)
   
-dipidolor %<>% as.matrix() %>% as_tibble() 
-dipidolor %<>% mutate_at(vars(`0_0`:`3_2`), as.integer) 
+dipidolor_qualitative <- dipidolor %>% dplyr::select(matches("ID|ja")) 
+colnames(dipidolor_qualitative)
+dipidolor_qualitative %<>% as.data.frame(as.matrix()) %>% 
+  mutate_at(vars(-ID), ~ as.integer(.)) %>% 
+  mutate(ID = as.character(ID)) %>% 
+  pivot_longer(-ID) %>% 
+  mutate(time = str_sub(name, 1, 3)) %>% 
+  group_by(ID, time) %>% 
+  summarise(value_qual = as.integer(sum(value, na.rm = TRUE))) %>% 
+  #rename(value_qual = value) %>% 
+  dplyr::select(ID, time, value_qual) %>% 
+  arrange(ID, time)
+dipidolor_qualitative$time %>% unique()
+head(dipidolor_qualitative, 10)
 
-#dipidolor  %<>%  mutate(prob_interv = 100*rowMeans( select(., colnames(dipidolor[,-1])) , na.rm = TRUE ))
-# ggplot(dipidolor, aes(x= prob_interv)) + geom_histogram()
-# nrow(filter(dipidolor, prob_interv > 0))
+dipidolor <- full_join(dipidolor_qualitative, dipidolor_quantitative) %>% 
+  #mutate_all(~ replace_na(., 0)) %>% 
+  dplyr::select(ID, time, everything()) %>% 
+  mutate(Dipidolor = pmax(value_qual,value_quan, na.rm = TRUE),
+         identity = (value_qual == value_quan))
 
-dipidolor %>% pivot_longer(-ID, names_to = "time", values_to = "dipidolor") %>% 
-  mutate(dipidolor = as.integer(dipidolor)) %>% View()
+nrow(filter(dipidolor, identity == FALSE))
+rm(dipidolor_qualitative, dipidolor_quantitative)
+
+dipidolor %<>% dplyr::select(ID, time, Dipidolor)
 
 
-ains <- spss_data_n158 %>% dplyr::select(matches("ID|meta|nov|ibu|volt|par")) %>%
-  select(-matches("4_")) 
-
-ains %>% select_at(.vars=numericVars) %>% 
-  summarise_all(.funs=var) %>%
-  filter_all(any_vars(. == 0))
+ains <- spss_data_n158 %>%
+  dplyr::select(matches("ID|meta|nov|ibu|volt|par|per")) %>%
+  dplyr::select(-matches("4_|5_|Vome|Nw")) %>% 
+  mutate(Awr_novalgin_mg = as.double(Awr_novalgin_mg)) %>% 
+  dplyr::select(-c(Novalgin_perioperativ_mg, matches("Abweichung"))) %>% 
+  mutate(
+    '0_0_Novalgin' = pmax(Awr_metamizol_mg, Awr_novalgin_mg, na.rm = TRUE) %>% 
+                      pmax(Novalgin_perioperativ_ja1, na.rm = TRUE),
+    '1_X_Novalgin' = pmax(Novalgin_tag_1_mg, `1_tag_novalgin_mg`, na.rm = TRUE)
+         ) %>% 
+  dplyr::select(-c(Awr_metamizol_mg, Awr_novalgin_mg, Novalgin_perioperativ_ja1,
+                   Novalgin_tag_1_mg, `1_tag_novalgin_mg`)) %>% 
+  select_all(~str_replace_all(., "paractamol", "Paracetamol")) %>% 
+  select_all(~str_replace_all(., "paracetamol", "Paracetamol")) %>% 
+  select_all(~str_replace_all(., "ibu", "Ibuprofen")) %>%
+  select_all(~str_replace_all(., "novalgin", "Metamizol")) %>%
+  select_all(~str_replace_all(., "Novalgin", "Metamizol")) %>%
+  select_all(~str_replace_all(., "voltaren", "Diclofenac")) %>% 
+  select_all(~str_replace_all(., "Op_tag_station", "0_X")) %>% 
+  select_all(~str_replace_all(., "Op_tag", "0_X")) %>% 
+  select_all(~str_replace_all(., "_mg", "")) %>% 
+  select_all(~str_replace_all(., "Awr", "0_0")) %>% 
+  select_all(~str_replace_all(., "tag", "X")) %>% 
+  dplyr::select(sort(names(.))) %>% dplyr::select(ID, everything()) %>% 
+  rowwise() %>% mutate_at( vars(-ID), ~ ifelse(is.na(.), NA, ifelse(. > 0, 1, .)) ) %>% 
+  pivot_longer(-ID) %>% 
+  separate(name, c("A", "B", "drug")) %>% 
+  pivot_wider(id_cols = c(ID, A, B), names_from = drug, values_from = value) %>% 
+  unite("time", A:B, na.rm = TRUE, remove = TRUE, sep = "_")
   
   
-  
-  transmute(ID = ID,
-    0_0_Novalgin = Novalgin_perioperativ_ja1,
-            
-    )
 
+
+colnames(ains)
+
+library(purrr)
+
+map_dfr(ains, ~ round(100*mean(is.na(.)),2)) %>%
+  t() %>% as.data.frame(stringsAsFactors = FALSE) %>% rownames_to_column() %>%
+  rename(column = rowname, percent_missing = V1) %>% arrange(percent_missing) %>%
+  filter(percent_missing < 90) %>% pull(column) -> cols_to_keep;
+medication <- select(medication, one_of(cols_to_keep)); rm(cols_to_keep)
+
+# examine the missing values, the distinct values 
+map_dfr(ains, ~ dplyr::n_distinct(.)) %>% 
+  t() %>% as.data.frame(stringsAsFactors = FALSE) %>% tibble::rownames_to_column() %>%
+  rename(column = rowname, distinct_values = V1) %>% 
+  full_join(
+    map_dfr(medication, ~ round(100*mean(is.na(.)),2)) %>% 
+      t() %>% as.data.frame(stringsAsFactors = FALSE) %>% rownames_to_column() %>%
+      rename(column = rowname, percent_missing = V1)
+  ) -> explore
 
 
 
@@ -115,6 +181,9 @@ processed_data_n158 <- select(spss_data_n158,
               matches(admit_expression),
              -matches(reject_expression)
               ) %>% 
+  
+  
+  
 
       select_all(~str_replace_all(., "tag_mo", "0")) %>%
       select_all(~str_replace_all(., "tag_mi", "1")) %>%
